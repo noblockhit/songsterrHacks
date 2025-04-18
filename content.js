2fe5d422193870111198b356813c6bf78ea4ba99
noblockhit <67117651+noblockhit@users.noreply.github.com>
2025-04-18 23:32:43 +0200

    .

--- content.js
+++ content.js
@@ -1,13 +1,15 @@
-const textSearch = "continue with interruptions";
 
-function removeOverlay() {
-    var lnks = document.getElementsByTagName("a");
-    for (var i = 0; i < lnks.length; i++) {
-        if (lnks[i].textContent == textSearch) {
-            lnks[i].click();
-            break;
+if (window.location.href.indexOf("songsterr.com") != -1) {
+    
+    const xPath = "/html/body/div[1]/div/div[5]/div[2]/div/p/a";
+    
+    function removeOverlay() {
+        var link = document.evaluate(xPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (link != null) {
+            document.evaluate(xPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
         }
     }
+    console.log("Songsterr Hacks: Removing overlay");
+    setInterval(removeOverlay, 10);
+   
 }
-
-setInterval(removeOverlay, 100);
