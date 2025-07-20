31fbfe60a1292574e8eee5279e131461dcd450d4
noblockhit <67117651+noblockhit@users.noreply.github.com>
2025-07-20 14:23:16 +0200

    added auto switching to original audio

--- content.js
+++ content.js
@@ -1,6 +1,27 @@
 
+function simulateKeyPress(character) {
+    var event = new KeyboardEvent('keydown', {
+        bubbles: true,
+        cancelable: true,
+        key: character,
+        charCode: character.charCodeAt(0),
+        keyCode: character.charCodeAt(0)
+    });
+    console.log("Simulating key press: " + character);
+    document.dispatchEvent(event);
+}
+
 if (window.location.href.indexOf("songsterr.com") != -1) {
     
+    setTimeout(() => {
+        // check aria-label of this xpath: /html/body/div/div/section/aside/div/div[1]/div[2]/div/div
+        var element = document.evaluate("/html/body/div/div/section/aside/div/div[1]/div[2]/div/div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (element && element.getAttribute("aria-label") === "Switch to original audio | Press V to toggle") {
+            simulateKeyPress("v");
+        }
+    }, 1000);
+    
+
     const xPath = "/html/body/div[1]/div/div[5]/div[2]/div/p/a";
     
     function removeOverlay() {
